11e7504707a6b2f163cf20f2c5bdaf62c292ab77
ddunaev247 <daniil.dunaev247@gmail.com>
2022-11-15 22:33:24 +0300

    done hw8

---
+++ JavaScript/homeworks/homework_8/script.js
@@ -0,0 +1,27 @@
+'use scrict'
+
+// task 1
+// let str = 'aaa@bbb@ccc'
+// console.log(str.replace(/@/g,'!'))
+
+// task 2
+// let str2 = 'aaa bbb ccc'
+// console.log(str2.replace(/bbb/g, ''))
+
+// task 3
+// let str3 = 'js'
+// let str4 = 'JS'
+// console.log(str3.toUpperCase(),str4.toLowerCase())
+
+// task 4
+// let str44 = 'sdsd' 
+// let str444 = '12aawwRR'
+// const shortenText = (str,n)=> str.length>n?str.slice(0,2)+'...':str
+// console.log(shortenText(str44,5),shortenText(str444,3))
+
+// task 5
+// let num = 379
+// console.log((num**0.5).toFixed(0),(num**0.5).toFixed(1),(num**0.5).toFixed(2))
+
+// task 6
+console.log((Math.random()*100).toFixed())
