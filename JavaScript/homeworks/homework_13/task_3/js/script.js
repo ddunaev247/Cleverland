f27f6421ea6fc714c59c41600fa7ae9c60a04601
ddunaev247 <daniil.dunaev247@gmail.com>
2022-12-06 14:50:23 +0300

    cut  hw13

--- JavaScript/homeworks/homework_13/task_3/js/script.js
+++
@@ -1,4 +0,0 @@
-'use scrict';
-let lists = [...document.querySelectorAll('ul > li')];
-colors = ['#87ceeb', '#008000', '#ffff00', '#ffa500'];
-lists.forEach((el, index) => (el.style.backgroundColor = `${colors[index]}`));
