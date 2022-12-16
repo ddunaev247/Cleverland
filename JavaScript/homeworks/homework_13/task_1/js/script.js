cc25f7760d1e59ae149324bd42ae7f85de6e4bc8
ddunaev247 <daniil.dunaev247@gmail.com>
2022-12-17 02:21:26 +0300

    done hw1

--- JavaScript/homeworks/homework_13/task_1/js/script.js
+++ JavaScript/homeworks/homework_13/task_1/js/script.js
@@ -1,12 +1,7 @@
 'use scrict';
-let rows = [...document.querySelectorAll('tr')];
-let count = 0;
-let count2 = rows[0].querySelectorAll('td').length - 1;
-let elems = [];
-rows.forEach(item => {
-    elems = [...item.querySelectorAll('td')];
-    elems[count].style.backgroundColor = 'red';
-    elems[count2].style.backgroundColor = 'red';
-    count++;
-    count2--;
-});
+const table = document.body.firstElementChild;
+
+for (let i = 0; i < table.rows.length; i++) {
+    let row = table.rows[i];
+    row.cells[i].style.backgroundColor = 'red';
+}
