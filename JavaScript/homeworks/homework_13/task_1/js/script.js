ebbaa61610fc987864c6a849af35bce3b0f153e9
ddunaev247 <daniil.dunaev247@gmail.com>
2022-12-26 01:29:48 +0300

    fix

--- JavaScript/homeworks/homework_13/task_1/js/script.js
+++ JavaScript/homeworks/homework_13/task_1/js/script.js
@@ -1,7 +1,8 @@
-'use scrict';
+'use strict';
 const table = document.body.firstElementChild;
 
 for (let i = 0; i < table.rows.length; i++) {
     let row = table.rows[i];
     row.cells[i].style.backgroundColor = 'red';
+    Array.from(row.cells).reverse()[i].style.backgroundColor = 'red';
 }
--- JavaScript/homeworks/homework_13/task_3/js/script.js
+++ JavaScript/homeworks/homework_13/task_3/js/script.js
@@ -1,4 +1,4 @@
 'use scrict';
 let lists = [...document.querySelectorAll('ul > li')];
-colors = ['#87ceeb', '#008000', '#ffff00', '#ffa500'];
+const colors = ['#87ceeb', '#008000', '#ffff00', '#ffa500'];
 lists.forEach((el, index) => (el.style.backgroundColor = `${colors[index]}`));
